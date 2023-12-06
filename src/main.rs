61d2ffeb8403a7e8e2042d2142479eb3e96fdb0f
MegenisJ <j.a.megenis@gmail.com>
2023-12-06 19:10:35 +0000

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,8 @@ fn main() {
     let mut total = 0;
     for line in fs::read_to_string("input.txt").unwrap().lines(){
 
-        //extract game number (only do this if we need it
-
         let mut game_number_section = line.split(":");
+        
         _=game_number_section.next();
         
         total += power_min_set_cubes(game_number_section.next().unwrap());
@@ -18,12 +17,18 @@ fn power_min_set_cubes(line : &str) -> i32 {
     let mut max_red = 0;
     let mut max_blue = 0;
     let mut max_green = 0;
+
     for game in line.split(";") {
         for colour in game.split(","){
+    
             let mut sections = colour.split(" ");
+            
             sections.next();
+            
             let x = sections.next().unwrap();
+            
             let quantity = x.parse::<i32>().unwrap();
+         
             match sections.next().unwrap()
             {
                 "red" => if quantity > max_red {max_red = quantity;} ,
@@ -31,8 +36,10 @@ fn power_min_set_cubes(line : &str) -> i32 {
                 "blue" =>  if quantity > max_blue {max_blue = quantity;},
                 _ => (),
             }
+
         }
     }
+
     return max_red *  max_blue * max_green ;
 }
 
